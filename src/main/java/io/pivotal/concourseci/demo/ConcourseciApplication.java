b9457d122eae54f474ffd2c9e4fc52ca275353a6
Pivotal Email Station <pivotalemailstation@MacBook-Air-108.local>
2017-02-28 17:30:25 +0000

    Initial commit

---
+++ src/main/java/io/pivotal/concourseci/demo/ConcourseciApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.concourseci.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcourseciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcourseciApplication.class, args);
+	}
+}
